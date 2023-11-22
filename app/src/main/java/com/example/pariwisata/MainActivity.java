81b4cffce2c08ad6dd3392d995b1d52aac5fe7be
T16 <rafikhoriul65@gmail.com>
2023-11-21 23:00:40 -0800

    Fragment

--- app/src/main/java/com/example/pariwisata/MainActivity.java
+++ app/src/main/java/com/example/pariwisata/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -43,5 +46,53 @@ public class MainActivity extends AppCompatActivity {
         fragmentManager.beginTransaction()
                 .replace(R.id.fragment_container, fragment)
                 .commit();
+    }
+    public void minggat1(View view) {
+        Uri uri = Uri.parse("https://wa.me/6281325154774");
+        Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+        startActivity(intent);
+
+    }
+
+    public void minggat2(View view) {
+        Uri uri = Uri.parse("https://wa.me/6281521563942");
+        Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+        startActivity(intent);
+    }
+
+    public void minggat3(View view) {
+        Uri uri = Uri.parse("https://wa.me/6285799788878");
+        Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+        startActivity(intent);
+    }
+
+    public void minggat4(View view) {
+        Uri uri = Uri.parse("https://wa.me/6281328662370");
+        Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+        startActivity(intent);
+    }
+
+    public void igesatu(View view) {
+        Uri uri = Uri.parse("https://www.instagram.com/rafiaslinofake/");
+        Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+        startActivity(intent);
+    }
+
+    public void igedua(View view) {
+        Uri uri = Uri.parse("https://www.instagram.com/yyowdnuvhereee/");
+        Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+        startActivity(intent);
+    }
+
+    public void igetiga(View view) {
+        Uri uri = Uri.parse("https://www.instagram.com/rasyad.mencarinama/");
+        Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+        startActivity(intent);
+    }
+
+    public void igeempat(View view) {
+        Uri uri = Uri.parse("https://www.instagram.com/drianvon_/");
+        Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+        startActivity(intent);
     }
 }
