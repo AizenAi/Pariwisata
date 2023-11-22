839062928adae1932ebfbd8b7e854a5faa874fb7
Rasyaadd <rasyadelgiva@gmail.com>
2023-11-22 09:02:57 +0700

    GetStarted Coy2

--- app/src/main/java/com/example/pariwisata/MainActivityGetStarted.java
+++ app/src/main/java/com/example/pariwisata/MainActivityGetStarted.java
@@ -2,13 +2,26 @@ package com.example.pariwisata;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
 public class MainActivityGetStarted extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_get_started);
+    }
+
+
+
+    public void btnLogin(View view) {
+        Intent intent = new Intent(MainActivityGetStarted.this, formlogin.class );
+        startActivity(intent);
+    }
+
+    public void pindah(View view) {
+        Intent intent = new Intent(MainActivityGetStarted.this, MainActivity.class);
+        startActivity(intent);
     }
 }
