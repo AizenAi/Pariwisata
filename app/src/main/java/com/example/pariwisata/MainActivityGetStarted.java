cb68061c7c2c4b7b8aa8c9553d576e33ab938217
T16 <rafikhoriul65@gmail.com>
2023-11-21 21:16:54 -0800

    Fragment

--- app/src/main/java/com/example/pariwisata/MainActivityGetStarted.java
+++ app/src/main/java/com/example/pariwisata/MainActivityGetStarted.java
@@ -15,12 +15,12 @@ public class MainActivityGetStarted extends AppCompatActivity {
 
 
 
-    public void btnLogin(View view) {
+    public void asjda(View view) {
         Intent intent = new Intent(MainActivityGetStarted.this, formlogin.class );
         startActivity(intent);
     }
 
-    public void pindah(View view) {
+    public void button(View view) {
         Intent intent = new Intent(MainActivityGetStarted.this, MainActivity.class);
         startActivity(intent);
     }
