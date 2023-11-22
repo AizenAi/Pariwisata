4990d9ad28a2c56a8245fa3bd45ed224e550d338
Aizen <rafikhoriul65@gmail.com>
2023-11-22 22:18:45 +0700

    Gweh njirr

--- app/src/main/java/com/example/pariwisata/MainActivity.java
+++ app/src/main/java/com/example/pariwisata/MainActivity.java
@@ -7,17 +7,39 @@ import androidx.fragment.app.FragmentManager;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int PICK_IMAGE_REQUEST = 1000;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.navView);
+        ViewGroup.LayoutParams layoutParams = imageView.getLayoutParams();
+        layoutParams.width = 500;
+        layoutParams.height = 500;
+        imageView.setLayoutParams(layoutParams);
+        Button button = findViewById(R.id.navView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iGallery = new Intent(Intent.ACTION_PICK);
+                iGallery.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivity(iGallery);
+            }
+        });
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.navView);
         bottomNavigationView.setOnItemSelectedListener(item -> {
