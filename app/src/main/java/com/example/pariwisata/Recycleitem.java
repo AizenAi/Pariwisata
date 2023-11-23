1a0e7278dc8ad24cfba3f9bbb8a6ea46e6c6ee58
Aizen <rafikhoriul65@gmail.com>
2023-11-23 22:27:27 +0700

    ProfilePage

---
+++ app/src/main/java/com/example/pariwisata/Recycleitem.java
@@ -0,0 +1,44 @@
+package com.example.pariwisata;
+
+public class Recycleitem {
+
+    public int imageView5;
+    public int imageView6;
+    public int imageView7;
+    public String textView10;
+    public String textView11;
+    public String textView12;
+
+    public int getImageView5() {
+        return imageView5;
+    }
+
+    public int getImageView6() {
+        return imageView6;
+    }
+
+    public int getImageView7() {
+        return imageView7;
+    }
+
+    public String getTextView10() {
+        return textView10;
+    }
+
+    public String getTextView11() {
+        return textView11;
+    }
+
+    public String getTextView12() {
+        return textView12;
+    }
+
+    public Recycleitem(int imageView5, int imageView6, int imageView7, String textView10, String textView11, String textView12) {
+        this.imageView5 = imageView5;
+        this.imageView6 = imageView6;
+        this.imageView7 = imageView7;
+        this.textView10 = textView10;
+        this.textView11 = textView11;
+        this.textView12 = textView12;
+    }
+}
