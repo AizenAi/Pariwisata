e397bd93946a134efb8d58a399f9597d40491385
T16 <rafikhoriul65@gmail.com>
2023-11-23 22:06:59 -0800

    Fragment

--- app/src/main/java/com/example/pariwisata/formlogin.java
+++ app/src/main/java/com/example/pariwisata/formlogin.java
@@ -1,12 +1,15 @@
 package com.example.pariwisata;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
-
 
 public class formlogin extends AppCompatActivity {
 
@@ -14,13 +17,41 @@ public class formlogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formlogin);
+
+        EditText editTextEmail = findViewById(R.id.editTextTextEmailAddress2);
+        EditText editTextName = findViewById(R.id.editTextText);
+
+        String enteredEmail = editTextEmail.getText().toString();
+        String enteredName = editTextName.getText().toString();
+
+        // Create a bundle and put the text in it with different keys
+        Bundle bundle = new Bundle();
+        bundle.putString("emailKey", enteredEmail); // Use a different key
+        bundle.putString("nameKey", enteredName); // Use a different key
+
+        // Create a new fragment instance and set the arguments
+        ProfileFragment fragment = new ProfileFragment();
+        fragment.setArguments(bundle);
+
+        // Start a fragment transaction
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+        // Replace the fragment_container with your fragment
+        transaction.replace(R.id.fragment_container, fragment);
+
+        // Add the transaction to the back stack (optional)
+        transaction.addToBackStack(null);
+
+        // Commit the transaction
+        transaction.commit();
     }
 
     public void masuklogin(View view) {
-            Intent intent = new Intent(formlogin.this, MainActivity.class );
-            startActivity(intent);
+        Intent intent = new Intent(formlogin.this, MainActivity.class);
+        startActivity(intent);
 
-            Toast toast  = Toast.makeText(this, "Anda Berhasil Masuk ", Toast.LENGTH_LONG);
-            toast.show();
-        }
+        Toast toast = Toast.makeText(this, "Anda Berhasil Masuk ", Toast.LENGTH_LONG);
+        toast.show();
+    }
 }
