ffafd7023767ade851adb3a673acbf68ca67db38
Rasyaadd <rasyadelgiva@gmail.com>
2023-11-24 13:42:31 +0700

    lalala

--- app/src/main/java/com/example/pariwisata/ActivityDalamNegara.java
+++ app/src/main/java/com/example/pariwisata/ActivityDalamNegara.java
@@ -2,7 +2,12 @@ package com.example.pariwisata;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class ActivityDalamNegara extends AppCompatActivity {
 
@@ -10,5 +15,24 @@ public class ActivityDalamNegara extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dalam_negara);
+        Button searchButtonn = findViewById(R.id.pindahGmaps);
+
+        searchButtonn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://www.google.com/maps/search/";
+                String u = "Raja Ampat";
+                String keyword = url + u;
+                Intent searchIntent = new Intent(Intent.ACTION_VIEW);
+
+                searchIntent.setData(Uri.parse(keyword));
+                startActivity(searchIntent);
+            }
+        });
+    }
+
+    public void pindahketu(View view) {
+        Intent i = new Intent(ActivityDalamNegara.this, dalamnegara2.class);
+        startActivity(i);
     }
 }
