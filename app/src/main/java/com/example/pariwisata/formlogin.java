115b65f4962d69e5d90f06ce0f1dec83260b745e
Aizen <rafikhoriul65@gmail.com>
2023-11-25 15:02:45 +0700

    ProfilePage

--- app/src/main/java/com/example/pariwisata/formlogin.java
+++ app/src/main/java/com/example/pariwisata/formlogin.java
@@ -18,33 +18,6 @@ public class formlogin extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formlogin);
 
-        EditText editTextEmail = findViewById(R.id.editTextTextEmailAddress2);
-        EditText editTextName = findViewById(R.id.editTextText);
-
-        String enteredEmail = editTextEmail.getText().toString();
-        String enteredName = editTextName.getText().toString();
-
-        // Create a bundle and put the text in it with different keys
-        Bundle bundle = new Bundle();
-        bundle.putString("emailKey", enteredEmail); // Use a different key
-        bundle.putString("nameKey", enteredName); // Use a different key
-
-        // Create a new fragment instance and set the arguments
-        ProfileFragment fragment = new ProfileFragment();
-        fragment.setArguments(bundle);
-
-        // Start a fragment transaction
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-
-        // Replace the fragment_container with your fragment
-        transaction.replace(R.id.fragment_container, fragment);
-
-        // Add the transaction to the back stack (optional)
-        transaction.addToBackStack(null);
-
-        // Commit the transaction
-        transaction.commit();
     }
 
     public void masuklogin(View view) {
