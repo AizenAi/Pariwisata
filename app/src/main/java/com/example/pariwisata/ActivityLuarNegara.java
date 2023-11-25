178a0341c4e2de115e4da39cf26399576f3d68b7
Rasyaadd <rasyadelgiva@gmail.com>
2023-11-25 13:51:10 +0700

    Banyak bgt ke favorit

--- app/src/main/java/com/example/pariwisata/ActivityLuarNegara.java
+++ app/src/main/java/com/example/pariwisata/ActivityLuarNegara.java
@@ -2,7 +2,11 @@ package com.example.pariwisata;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivityLuarNegara extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class ActivityLuarNegara extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_luar_negara);
+        Button searchButtonn = findViewById(R.id.pindahGmaps);
+
+
+        searchButtonn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View    view) {
+                String url = "https://www.google.com/maps/search/";
+                String u = "Disney Tokyo";
+                String keyword = url + u;
+                Intent searchIntent = new Intent(Intent.ACTION_VIEW);
+
+                searchIntent.setData(Uri.parse(keyword));
+                startActivity(searchIntent);
+            }
+        });
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+    }
+
+    public void pindahketu(View view) {
+        Intent i = new Intent(ActivityLuarNegara.this, LuarNegaraTU.class);
+        startActivity(i);
     }
 }
